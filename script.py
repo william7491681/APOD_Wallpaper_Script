070094762af956482a46c03e8380230d0bb33e05
William Dines <williamdines@rocketmail.com>
2024-05-24 12:42:13 -0500

    Add variables for monitor width/height and a specified file save path

--- script.py
+++ script.py
@@ -4,6 +4,13 @@ import os
 from PIL import Image
 import datetime
 import ctypes
+import json
+
+with open("userDefinedVariables.json", "r") as userDefinedVariables:
+  userDefinedVariables = json.load(userDefinedVariables)
+  monitorWidth = userDefinedVariables['MONITOR_WIDTH']
+  monitorHeight = userDefinedVariables['MONITOR_HEIGHT']
+  fileSavePath = userDefinedVariables['FILE_SAVE_PATH']
 
 today = datetime.datetime.now().strftime("%Y-%m-%d").replace("/", "-")
 eightDaysAgo = (datetime.datetime.now() - datetime.timedelta(days=8)).strftime("%Y-%m-%d").replace("/", "-")
@@ -26,20 +33,20 @@ imgs = []
 for i in response:
   if "thumbnail_url" in i:
     img = Image.open(requests.get(i["thumbnail_url"], stream=True).raw)
-    img = img.resize((1920//3, 1080//3), Image.Resampling.LANCZOS)
+    img = img.resize((monitorWidth//3, monitorHeight//3), Image.Resampling.LANCZOS)
     imgs.append(img)
   elif "hdurl" in i:
     img = Image.open(requests.get(i["hdurl"], stream=True).raw)
-    img = img.resize((1920//3, 1080//3), Image.Resampling.LANCZOS)
+    img = img.resize((monitorWidth//3, monitorHeight//3), Image.Resampling.LANCZOS)
     imgs.append(img)
   elif "url" in i:
     img = Image.open(requests.get(i["url"], stream=True).raw)
-    img = img.resize((1920//3, 1080//3), Image.Resampling.LANCZOS)
+    img = img.resize((monitorWidth//3, monitorHeight//3), Image.Resampling.LANCZOS)
     imgs.append(img)
   else:
-    imgs.append(Image.new('RGB', (1920//3, 1080//3)))
+    imgs.append(Image.new('RGB', (monitorWidth//3, monitorHeight//3)))
 
-finalImg = Image.new('RGB', (1920, 1080))
+finalImg = Image.new('RGB', (monitorWidth, monitorHeight))
 finalImg.paste(imgs[0], (0, 0))
 finalImg.paste(imgs[1], (640, 0))
 finalImg.paste(imgs[2], (1280, 0))
@@ -52,8 +59,7 @@ finalImg.paste(imgs[6], (0, 720))
 finalImg.paste(imgs[7], (640, 720))
 finalImg.paste(imgs[8], (1280, 720))
 
-savePath = r"C:\Users\willi\Pictures\APOD Pics\collage.jpg"
-if os.path.exists(savePath):
-  os.remove(savePath)
-finalImg.save(savePath)
-ctypes.windll.user32.SystemParametersInfoW(20, 0, savePath , 0)
+if os.path.exists(fileSavePath):
+  os.remove(fileSavePath)
+finalImg.save(fileSavePath)
+ctypes.windll.user32.SystemParametersInfoW(20, 0, fileSavePath , 0)
